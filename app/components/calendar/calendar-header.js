5e0bd02a00823406a92b85a237987ee16b55a3a5
Web Dev <jgrewal@Jagnoors-MacBook-Pro.local>
2019-11-25 14:38:01 -0500

    updated calendar-header

--- app/components/calendar/calendar-header.js
+++ app/components/calendar/calendar-header.js
@@ -5,16 +5,13 @@ class Calendar_Header extends React.Component {
     constructor (props) {
         super (props);
         this.state = store.getState();
-
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange (evt) {
-        var date = [evt.target.value, this.state.calendar.calendar_date[1], this.state.calendar.calendar_date[2]];
-        
+        var date = [evt.target.value, this.state.calendar.calendar_date[1], this.state.calendar.calendar_date[2]];       
         store.dispatch(set_calendar_date(date));
-        store.dispatch(set_active_month(date));
-  
+        store.dispatch(set_active_month(date)); 
         evt.preventDefault();
     }
 
@@ -34,7 +31,6 @@ class Calendar_Header extends React.Component {
                         <input name="year" type="number" value={this.state.calendar.calendar_date[0]} onChange={this.handleChange} />
                     </form>
                 </div>
-
                 <div id="title">
                     Calendar
                 </div>
