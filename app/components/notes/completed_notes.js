ffade90286a2157c8fc96923ab2e188aef75a0ef
Web Dev <jgrewal@Jagnoors-MacBook-Pro.local>
2019-11-27 17:10:20 -0500

    complete note css, marks incomplete, moves task to active component

--- app/components/notes/completed_notes.js
+++ app/components/notes/completed_notes.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import store from '../../store/store.js';
+import store, {mark_note_incomplete} from '../../store/store.js';
 
 class Completed_Notes extends React.Component {
     constructor (props) {
         super (props);
         this.state = store.getState();
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange (evt) {
+        var note_id = evt.target.value;
+        if (evt.target.checked === false) {
+            store.dispatch(mark_note_incomplete(note_id));
+        }
     }
 
     componentWillMount() {
@@ -23,8 +31,14 @@ class Completed_Notes extends React.Component {
                 {
                     completed_notes_on_date ? completed_notes_on_date.map((note, i) => {
                         return (
-                            <div key={i}>
-                                {note.name}
+                            <div className='task incomplete' key={i}>
+                                
+                                <div>
+                                    {note.name}
+                                </div>
+                                <div>
+                                    <input name = "marked_complete" type="checkbox" value={note.id} onChange = {this.handleChange} checked/>
+                                </div>
                             </div>
                         )
                     }) : (null)
