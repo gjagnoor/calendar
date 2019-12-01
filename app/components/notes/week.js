424dbceaf8175a45772cceec60b9e1a556886b76
Web Dev <jgrewal@Jagnoors-MacBook-Pro.local>
2019-11-30 20:19:05 -0500

    week component works with react redux

--- app/components/notes/week.js
+++ app/components/notes/week.js
@@ -1,51 +1,24 @@
 import React from 'react';
-import store, { set_calendar_date, update_note } from '../../store/store.js';
-import { ECONNABORTED } from 'constants';
+import { set_calendar_date, update_note } from '../../store/store.js';
+import {connect} from 'react-redux';
 
 class Week extends React.Component {
     constructor (props) {
         super(props);
-        this.state = store.getState();
-        this.handleClick = this.handleClick.bind(this);
-        this.handleDrop = this.handleDrop.bind(this);
     }
 
-    handleClick (evt) {
-        var date_selected = evt.target.id;
-        store.dispatch(set_calendar_date(date_selected));
-    }
-
-    handleDrop (evt) {
-        var move_to_date = evt.target.id;
-        evt.preventDefault();
-        var note_id = evt.dataTransfer.getData("id");
-        store.dispatch(update_note(note_id, "due_date", move_to_date));
-    }
-
-    handleAllowDrop (evt) {
-        evt.preventDefault();
-    }
-
-    componentDidMount() {
-        this.unsubscribe = store.subscribe(() => store.getState());
-    }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
     render () {
-        console.log(this.state.calendar.week)
         return (
             <div className="flex-row-center">
                 {
-                    this.state.calendar.week.map((day, i) => {
+                    this.props.week.map((day, i) => {
                         return (
-                            <div key={i} className="snuggle-fit" id={day} onDrop={this.handleDrop} onDragOver={this.handleAllowDrop}>
+                            <div key={i} className="snuggle-fit" id={day} onDrop={this.props.handleDrop} onDragOver={this.props.handleAllowDrop}>
                                 <div className="weekday">
                                     {day.split(" ")[0]}
                                 </div>
                                 {/* date prints with a space on one line when logged */}
-                                <div className = "weekdate" id={day} onClick={this.handleClick}>
+                                <div className = "weekdate" id={day} onClick={this.props.handleClick}>
                                     {day.split(" ").slice(1,3).join(" ")}
                                 </div>
                             </div>
@@ -57,4 +30,28 @@ class Week extends React.Component {
     }
 }
 
-export default Week;
+const mapStateToProps = (state) => {
+    return {
+        week: state.calendar.week
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleClick (evt) {
+            var date_selected = evt.target.id;
+            return dispatch(set_calendar_date(date_selected));
+        },   
+        handleDrop (evt) {
+            var move_to_date = evt.target.id;
+            evt.preventDefault();
+            var note_id = evt.dataTransfer.getData("id");
+            return dispatch(update_note(note_id, "due_date", move_to_date));
+        },    
+        handleAllowDrop (evt) {
+            evt.preventDefault();
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Week);
